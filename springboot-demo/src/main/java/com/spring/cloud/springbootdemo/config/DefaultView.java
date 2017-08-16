aab73ae2b5e7a0af2886f417f4862c57d47bf85b
wucy <wucy@drore.com>
2017-08-16 16:20:18 +0800

    sdk

---
+++ springboot-demo/src/main/java/com/spring/cloud/springbootdemo/config/DefaultView.java
@@ -0,0 +1,39 @@
+package com.spring.cloud.springbootdemo.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: wcy
+ * Date: 2017/8/15
+ * Time: 15:58
+ */
+@Configuration
+public class DefaultView extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureMessageConverters(
+            List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        //处理中文乱码问题
+        List<MediaType> fastMediaTypes = new ArrayList<>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastConverter.setSupportedMediaTypes(fastMediaTypes);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        converters.add(fastConverter);
+    }
+
+
+}
--- springboot-demo/src/main/java/com/spring/cloud/springbootdemo/config/DruidStatFilter.java
+++ springboot-demo/src/main/java/com/spring/cloud/springbootdemo/config/DruidStatFilter.java
@@ -12,7 +12,7 @@ import javax.servlet.annotation.WebInitParam;
  */
 @WebFilter(filterName = "druidWebStatFilter", urlPatterns = "/*",
         initParams = {
-                @WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")// 忽略资源
+                @WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*,/swagger*")// 忽略资源
         }
 )
 public class DruidStatFilter extends WebStatFilter {
